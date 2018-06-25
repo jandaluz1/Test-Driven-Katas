463b663957ac77b5ab6b7593b730439bc33e172b
Joe Andaluz <jandaluz1@gmail.com>
2018-06-25 12:01:01 -0400

    two Sum tests passed

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,15 @@
-function twoSum(array,target){
-  let result;
-  for(let i = 0; i < array.length; i ++){
-    for(let j = 0; j < array.length; j ++){
-      if(array[i] + array[j] === target && i !== j ){
-        result = [i,j]
+function twoSum(array, target) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target && i !== j) {
+        result.push(i);
+        result.push(j);
         return result;
       }
     }
   }
+  return result;
 }
 
 module.exports = twoSum;
--- twoSum.spec.js
+++ twoSum.spec.js
@@ -3,6 +3,12 @@ const twoSum = require('./twoSum');
 
 describe('twoSum', () => {
   it('Returns index of elements that adds up to target', () => {
-    expect(twoSum([1,2,3],5)).to.equal([1,2]);
+    expect(twoSum([1, 2, 3], 5)).to.deep.equal([1, 2]);
   });
-})
+  it('Returns index of elements that adds up to target', () => {
+    expect(twoSum([1, 2, 3, 4, 5, 6], 11)).to.deep.equal([4, 5]);
+  });
+  it('Returns empty array if there is no solution', () => {
+    expect(twoSum([1, 2, 3], 6)).to.deep.equal([]);
+  });
+});
